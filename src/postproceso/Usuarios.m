dc95b45fcbea5413adeb457a7b0db91c97fa35ce
jdmontoya1 <74557185+jdmontoya1@users.noreply.github.com>
2021-05-24 16:13:50 -0500

    Corrección_postpreoceso
    Cambios en las funciones y aplicación de la fase postproceso

--- src/postproceso/Usuarios.m
+++ src/postproceso/Usuarios.m
@@ -1,6 +1,10 @@
 
-function [D1,P1,D2,P2,D3,P3,D4,P4,D5,P5,D6,P6,D7,P7,D8,P8,D9,P9,D10,P10,D11,P11,D12,P12,D13,P13,D14,P14,D15,P15,D16,P16,D17,P17,D18,P18,D19,P19,D20,P20] = Usuarios(Y)
+function [D,P,D01,P01,D02,P02,D03,P03,D1,P1,D2,P2,D3,P3,D4,P4,D5,P5,D6,P6,D7,P7,D8,P8,D9,P9,D10,P10,D11,P11,D12,P12,D13,P13,D14,P14,D15,P15,D16,P16,D17,P17,D18,P18,D19,P19,D20,P20] = Usuarios(Y)
 %Ocupante del vehículo
+S=find(Y.Ocupante_del_vehiculo==0);
+D=(1/1000)*sum(Y.Distancia(S));
+P=(D*100)/(1/1000*sum(Y.Distancia));
+
 S1=find(Y.Ocupante_del_vehiculo==1);
 D1=(1/1000)*sum(Y.Distancia(S1));
 P1=(D1*100)/(1/1000*sum(Y.Distancia));
@@ -22,6 +26,10 @@ D5=(1/1000)*sum(Y.Distancia(S5));
 P5=(D5*100)/(1/1000*sum(Y.Distancia));
             
 %Motociclista
+S01=find(Y.Motociclista==0);
+D01=(1/1000)*sum(Y.Distancia(S01));
+P01=(D01*100)/(1/1000*sum(Y.Distancia));
+
 S6=find(Y.Motociclista==1);
 D6=(1/1000)*sum(Y.Distancia(S6));
 P6=(D6*100)/(1/1000*sum(Y.Distancia));
@@ -43,6 +51,10 @@ D10=(1/1000)*sum(Y.Distancia(S10));
 P10=(D10*100)/(1/1000*sum(Y.Distancia));
 
 %Ciclista
+S02=find(Y.Ciclista==0);
+D02=(1/1000)*sum(Y.Distancia(S02));
+P02=(D02*100)/(1/1000*sum(Y.Distancia));
+
 S11=find(Y.Ciclista==1);
 D11=(1/1000)*sum(Y.Distancia(S11));
 P11=(D11*100)/(1/1000*sum(Y.Distancia));
@@ -64,6 +76,10 @@ D15=(1/1000)*sum(Y.Distancia(S15));
 P15=(D15*100)/(1/1000*sum(Y.Distancia));
             
 %Peatón
+S03=find(Y.Peaton==0);
+D03=(1/1000)*sum(Y.Distancia(S03));
+P03=(D03*100)/(1/1000*sum(Y.Distancia));
+
 S16=find(Y.Peaton==1);
 D16=(1/1000)*sum(Y.Distancia(S16));
 P16=(D16*100)/(1/1000*sum(Y.Distancia));
